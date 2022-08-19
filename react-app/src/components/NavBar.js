9ca2206b52909e316eb2f92f734b76a5d9e07f93
Ben Durham <oorazugoku@gmail.com>
2022-08-19 17:23:20 -0500

    CSS folder creation

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,24 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './CSS/NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+    <nav className='NavBar-Container'>
+      <NavLink to='/' exact={true} activeClassName='active'>
+        Home
+      </NavLink>
+      <NavLink to='/login' exact={true} activeClassName='active'>
+        Login
+      </NavLink>
+      <NavLink to='/sign-up' exact={true} activeClassName='active'>
+        Sign Up
+      </NavLink>
+      <NavLink to='/users' exact={true} activeClassName='active'>
+         Users
+      </NavLink>
+      <LogoutButton />
     </nav>
   );
 }
