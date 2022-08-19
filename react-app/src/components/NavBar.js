814562e568f39a700d4746c5b85dd5abc5eb576c
Ben Durham <oorazugoku@gmail.com>
2022-08-19 18:14:11 -0500

    Images folder

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import './CSS/NavBar.css';
+import svg1 from './Images/svgexport-5.svg'
 
 const NavBar = () => {
   return (
     <nav className='NavBar-Container'>
-      <NavLink to='/' exact={true} activeClassName='active'>
-        Home
-      </NavLink>
-      <NavLink to='/login' exact={true} activeClassName='active'>
-        Login
-      </NavLink>
-      <NavLink to='/sign-up' exact={true} activeClassName='active'>
-        Sign Up
-      </NavLink>
-      <NavLink to='/users' exact={true} activeClassName='active'>
-         Users
-      </NavLink>
-      <LogoutButton />
+      <div className='home-button'>
+        <NavLink to='/' exact={true} activeClassName='active'>Home</NavLink>
+      </div>
+      <div>
+        <NavLink to='/login' exact={true} activeClassName='active'>Login</NavLink>
+        <NavLink to='/sign-up' exact={true} activeClassName='active'>Sign Up</NavLink>
+        {/* <NavLink to='/users' exact={true} activeClassName='active'>Users</NavLink> */}
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
