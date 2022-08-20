5e4a83010f1f1294f7ce79931aee5d195f241781
lynnettenguyen <nguyelynne@gmail.com>
2022-08-20 16:24:03 -0700

    migrated server

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,7 +4,7 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo User', email='demo@aa.io', password='password', img_url='')
+        username='Demo User', email='demo@aa.io', password='password')
     alexZ = User(
         username='Alex Z', email='alexZ@aa.io', password='password')
     benD = User(
