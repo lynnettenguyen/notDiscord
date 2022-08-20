d204538d21aba9bc14412a9ace8587fdbc9b9cec
lynnettenguyen <nguyelynne@gmail.com>
2022-08-20 15:18:29 -0700

    user seeds

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    img_url = db.Column(db.String(255))
 
     @property
     def password(self):
