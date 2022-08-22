6ef7a5434278505345b05b860e6641f9d97955f5
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-08-22 14:33:38 -0700

    Switching over to websockets

--- app/api/direct_message_routes.py
+++ app/api/direct_message_routes.py
@@ -5,6 +5,11 @@ from .auth_routes import login, validation_errors_to_error_messages
 from app.models import db, DirectMessage, User, DirectChat
 from app.forms import ServerForm, ChannelForm
 
-servers = Blueprint('direct_messages', __name__)
+direct_messages = Blueprint('direct_messages', __name__)
 
-# hi 
+
+@direct_messages.route("/<int:sender_id>")
+@login_required
+# get all direct messages by direct_chat_id
+def direct_messages_by_id(sender_id):
+  pass
