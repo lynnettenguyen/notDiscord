4f8e368680565ee7848de8335763f63eeed0435f
Ben Durham <oorazugoku@gmail.com>
2022-08-22 03:45:38 -0500

    adding layers to Home Page

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -5,6 +5,11 @@ import '../CSS/HomePage.css';
 import HomeLogo from '../CSS/images/notDiscord.png'
 import leftImage from '../CSS/images/svgexport-5.svg'
 import rightImage from '../CSS/images/svgexport-6.svg'
+import part3 from '../CSS/images/threePart.svg'
+import part4 from '../CSS/images/fourPart.svg'
+import part5 from '../CSS/images/fivePart.svg'
+import part6 from '../CSS/images/sixPart.svg'
+
 
 const HomePage = () => {
     const user = useSelector(state => state.session.user);
@@ -29,6 +34,7 @@ const HomePage = () => {
         </div>
         <div className='HomePage-background'>
             <img src={leftImage} className='HomePage-left-image'/>
+            <div className='space'/>
             <div className='HomePage-text'>
                 <h1>
                     IMAGINE A PLACE...
@@ -39,6 +45,22 @@ const HomePage = () => {
                 </span>
             </div>
             <img src={rightImage} className='HomePage-right-image'/>
+        </div>
+        <div className='HomePage-3rd-part'>
+            <img src={part3}/>
+            <span>TEXT</span>
+        </div>
+        <div className='HomePage-4th-part'>
+            <span>TEXT</span>
+            <img src={part4}/>
+        </div>
+        <div className='HomePage-5th-part'>
+            <img src={part5}/>
+            <span>TEXT</span>
+        </div>
+        <div className='HomePage-6th-part'>
+            <span>TEXT</span>
+            <img src={part6}/>
         </div>
         </>
     )
