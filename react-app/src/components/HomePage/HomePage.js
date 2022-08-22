d59bf3e04206921a723531a0cde3480b66bec1ce
Ben Durham <oorazugoku@gmail.com>
2022-08-22 04:59:54 -0500

    fixed all backgrounds buttons and styling

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -21,18 +21,15 @@ const HomePage = () => {
 
 
     return (
-        <>
-        <div className='HomePage-background-container'>
+        <div className='HomePage-container'>
+        <div className='HomePage-background'/>
             <nav className='NavBar-Container'>
-                <div className='home-button'>
-                <NavLink to='/'><img className='home-logo' src={HomeLogo}/></NavLink>
-                </div>
-                <div>
-                    <NavLink className='login-button' to='/login'>Login</NavLink>
+                <div className='NavBar-buttons'>
+                <div><NavLink exact to='/'><img className='home-logo' src={HomeLogo}/></NavLink></div>
+                <div><NavLink className='login-button' to='/login'>Login</NavLink></div>
                 </div>
             </nav>
-        </div>
-        <div className='HomePage-background'>
+        <div className='HomePage-heading-container'>
             <img src={leftImage} className='HomePage-left-image'/>
             <div className='HomePage-text'>
                 <h1>
@@ -47,21 +44,60 @@ const HomePage = () => {
         </div>
         <div className='HomePage-3rd-part'>
             <img src={part3}/>
-            <span>TEXT</span>
+            <span>
+                <h1>
+                    Create an invite-only place where you belong
+                </h1>
+                <span>
+                Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
+                </span>
+            </span>
         </div>
         <div className='HomePage-4th-part'>
-            <span>TEXT</span>
+            <span>
+                <h1>
+                    Where hanging out is easy
+                </h1>
+                <span>
+                    Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.
+                </span>
+            </span>
             <img src={part4}/>
         </div>
         <div className='HomePage-5th-part'>
             <img src={part5}/>
-            <span>TEXT</span>
+            <span>
+                <h1>
+                    From few to a fandom
+                </h1>
+                <span>
+                    Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.
+                </span>
+            </span>
         </div>
         <div className='HomePage-6th-part'>
-            <span>TEXT</span>
+            <span>
+                <h1>
+                    RELIABLE TECH FOR STAYING CLOSE
+                </h1>
+                <span>
+                    Low-latency voice and video feels like you’re in the same room. Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.
+                </span>
+            </span>
             <img src={part6}/>
         </div>
-        </>
+        <div className='HomePage-bottom-div'>
+            <div className='bottom-divider'/>
+            <div className='bottom-nav-container'>
+                <div className='left'>
+                    <NavLink to='/'><img className='home-logo' src={HomeLogo}/></NavLink>
+                </div>
+                <div className='right'>
+                    <NavLink className='signup-button' to='/login'>Login</NavLink>
+                </div>
+            </div>
+        </div>
+        </div>
     )
 }
 
