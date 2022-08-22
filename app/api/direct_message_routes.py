5cc9b944e0d6e5495ebdb7a1138c7ad26f71ebc7
Antony Pizarro <apizarro23@live.com>
2022-08-22 17:00:20 -0400

    started direct messages backend

---
+++ app/api/direct_message_routes.py
@@ -0,0 +1,8 @@
+import json
+from flask import Blueprint, jsonify, request
+from flask_login import current_user, login_required
+from .auth_routes import login, validation_errors_to_error_messages
+from app.models import db, DirectMessage, User, DirectChat
+from app.forms import ServerForm, ChannelForm
+
+servers = Blueprint('direct_messages', __name__)
