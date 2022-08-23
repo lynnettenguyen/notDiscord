53aff4e13d53d8c1197d9eac11b271c55e599f8f
Ben Durham <oorazugoku@gmail.com>
2022-08-23 18:21:19 -0500

    channels merge

--- app/api/channel_message_routes.py
+++ app/api/channel_message_routes.py
@@ -9,6 +9,8 @@ def channel_messages_by_channel(channel_id):
   cms = ChannelForm.query.all(channel_id)
   return jsonify(cms.to_dict())
 
+
+
 @channel_messages.route("/<int:channel_id>", methods=['POST'])
 @login_required
 def create_new_channel_message():
