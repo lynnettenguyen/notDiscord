6d3df3ab54dacf9935d4ec2616107bb27071c392
Antony Pizarro <apizarro23@live.com>
2022-08-23 11:23:44 -0400

    commented out direct and channel messages from __init__. still needs websockets

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.server_routes import servers
-from .api.direct_message_routes import direct_messages
-from .api.channel_message_routes import channel_messages
+# from .api.direct_message_routes import direct_messages
+# from .api.channel_message_routes import channel_messages
 
 from .seeds import seed_commands
 
@@ -39,8 +39,8 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(servers, url_prefix='/api/servers')
-app.register_blueprint(direct_messages, url_prefix='/api/direct_messages')
-app.register_blueprint(channel_messages, url_prefix='/api/channel_messages')
+# app.register_blueprint(direct_messages, url_prefix='/api/direct_messages')
+# app.register_blueprint(channel_messages, url_prefix='/api/channel_messages')git brnahc
 db.init_app(app)
 Migrate(app, db)
 
