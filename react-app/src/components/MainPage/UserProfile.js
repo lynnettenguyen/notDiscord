78448c9b63731bf753cf571011c4691cb8904308
lynnettenguyen <nguyelynne@gmail.com>
2022-08-27 19:12:28 -0700

    user profile

--- react-app/src/components/MainPage/UserProfile.js
+++ react-app/src/components/MainPage/UserProfile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/session";
 import "../CSS/UserProfile.css";
@@ -7,19 +7,19 @@ import "../CSS/UserProfile.css";
 const UserProfile = () => {
   const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
-  const onLogout = async (e) => {
-    await dispatch(logout());
+  const history = useHistory()
+
+  const onLogout = async () => {
+    dispatch(logout());
+    history.push('/')
   };
-  if (!user) {
-    return <Redirect to='/' />
-  }
 
   return (
     <div className="current-user-profile">
       <div className="user-pfp-outer">
-        <img src={user.profile_pic} alt='user-pfp-inner' className='current-user-profile-pic'></img>
+        <img src={user?.profile_pic} alt='user-pfp-inner' className='current-user-profile-pic'></img>
         <div className="username-outer">
-          {user.username}
+          {user?.username}
         </div>
       </div>
       <div className="logout-button-outer">
